aa954ef28a43a1004f3eb949fddc1f9be6edca7f
Ross Morgan <ross.morgan933@gmail.com>
2024-10-14 19:59:03 -0500

    database created

---
+++ Data/RSVPData.cs
@@ -0,0 +1,15 @@
+﻿using _26_28sweNamelessBE.Models;
+
+namespace _26_28sweNamelessBE.Data
+{
+    public class RSVPData
+    {
+        public static List<RSVP> RSVPs = new()
+        {
+            new(){  Id = 1, EventId = 101, Uid = "V4ZZtExf09dyP1GLH7Yhz7QqiOq2"},
+            new(){  Id = 2, EventId = 102, Uid = "ZpQoucFlCVNP5c0WunWKIi0mVKE3"},
+            new(){  Id = 3, EventId = 103, Uid = "Yy2T2FeWvZdd4W8epNHt37AId6J2"},
+
+        };
+    }
+}
